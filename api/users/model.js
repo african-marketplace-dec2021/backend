09e122e8412f7e2fc93fbae263951ddd8f05e9ee
beefybroccoli <tedkimemail@yahoo.com>
2021-12-18 04:47:49 -0800

    ...

--- api/users/model.js
+++ api/users/model.js
@@ -11,7 +11,8 @@ async function add(obj){
     // console.log("sequence = ", sequence);
     // console.log("last_value = ", last_value);
     // // =  [ { last_value: '5', log_cnt: '32', is_called: true } ]
-    const result = await db('users').insert({...obj, id:"nextval(`users_id_seq`)"});
+    // const result = await db('users').insert({...obj, id:"nextval(`users_id_seq`)"});
+    const result = await db('users').insert({...obj});
     return await db('users').where("id", last_value+1);
 }
 
