2fe14473b3a412699dc13f5e4fbb4e210c6f600d
beefybroccoli <tedkimemail@yahoo.com>
2021-12-18 16:23:00 -0800

    ...

--- api/server.js
+++ api/server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const server = express();
+const helmet = require("helmet");
+const cors = require("cors");
 const routerUsers = require("./users/router");
+const {errorHandler} = require("./errorHandler");
 
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
 
 server.get("/", (req, res)=>{
     res.status(200).json({message:"hello world from African Marketplace Dec 2021"});
@@ -12,5 +17,7 @@ server.use("/api/users", routerUsers);
 server.use('*', (req, res)=>{
     res.status(404).json({message:`invalid path ${req.path}`});
 })
+
+server.use(errorHandler);
 
 module.exports = server;
