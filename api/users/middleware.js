f99a8beb8f86c1d0bba50e80be75fa0ecb1db1ab
beefybroccoli <tedkimemail@yahoo.com>
2021-12-18 16:59:10 -0800

    ...

--- api/users/middleware.js
+++ api/users/middleware.js
@@ -1,10 +1,10 @@
 const model = require("./model");
-const {isEmptyArray, isUndefined} = require("../helper");
+const {isEmptyArray, isUndefined, isNumber} = require("../helper");
 
 async function verifyExistingId (req, res, next){
     try{
         const {id} = req.params;
-        if (isUndefined(id) || ){
+        if (isUndefined(id) || isNumber(id) === false){
             res.status(400).json({message:"require id"});
         }else {
             const array = await model.getById(id);
@@ -21,7 +21,8 @@ async function verifyExistingId (req, res, next){
 
 async function verifyNewObject (req, res, next){
     try{
-
+        //implement verify new object
+        next();
     }catch(err){
         next(err);
     }
