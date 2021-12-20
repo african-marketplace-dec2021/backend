fc6fb3021a91fb6ffb748c8ae4e30c95267bf6c4
beefybroccoli <tedkimemail@yahoo.com>
2021-12-20 02:32:50 -0800

    ...

--- database/migrations/20211220092100_06-transaction-table.js
+++ database/migrations/20211220092100_06-transaction-table.js
@@ -5,12 +5,10 @@ exports.up = function(knex, Promise) {
             table.integer("quantity").notNull();
 
             table.integer("product_id").notNull();
-            table.integer("category_id").notNull();
             table.integer("order_id").notNull();
 
-            // table.foreign("product_id").references("id").inTable("products");
-            // table.foreign("category_id").references("id").inTable("categories");
-            // table.foreign("order_id").references("id").inTable("orders");
+            table.foreign("product_id").references("id").inTable("products");
+            table.foreign("order_id").references("id").inTable("orders");
         })
 };
 
--- database/seeds/06-transactions-table.js
+++ database/seeds/06-transactions-table.js
@@ -4,11 +4,11 @@ exports.seed = function(knex) {
     // .truncate()
     .then(function () {
       return knex('transactions').insert([
-        {id: 1, quantity:1, product_id:1,category_id:1,order_id:1},
-        {id: 2, quantity:2, product_id:2,category_id:2,order_id:1},
-        {id: 3, quantity:3, product_id:3,category_id:3,order_id:1},
-        {id: 4, quantity:4, product_id:4,category_id:4,order_id:1},
-        {id: 5, quantity:5, product_id:5,category_id:5,order_id:1},
+        {id: 1, quantity:1, product_id:1, order_id:1},
+        {id: 2, quantity:2, product_id:2, order_id:1},
+        {id: 3, quantity:3, product_id:3, order_id:1},
+        {id: 4, quantity:4, product_id:4, order_id:1},
+        {id: 5, quantity:5, product_id:5, order_id:1},
       ]);
     });
 };
